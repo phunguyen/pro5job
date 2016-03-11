dc51ef82e2520bea6b7d98dbf81e2a5d76f0caf8
baphong <baphong80@gmail.com>
2016-03-11 10:43:50 +0700

    go o day a, ok

--- application/views/header.php
+++ application/views/header.php
@@ -17,12 +17,8 @@ $baseUrl = base_url();
 			</div>
 			<div id="navbar" class="collapse navbar-collapse">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="<?php echo $baseUrl;?>aboutus"><span class="glyphicon glyphicon-home"></span> Giới thiệu</a></li>
-					<li><a href="<?php echo $baseUrl;?>job"><span class="glyphicon glyphicon-bullhorn"></span> Tuyển dụng</a></li>
-					<li><a href="tuyendung.html"><span class="glyphicon glyphicon-bell"></span> Tìm việc</a></li>
-					<li><a href="<?php echo $baseUrl;?>filter"><span class="glyphicon glyphicon-filter"></span> Bộ lọc</a></li>
+					<li><a href="<?php echo $baseUrl;?>aboutus"><span class="glyphicon glyphicon-home"></span> Giới thiệu</a></li>					
 					<li><a href="<?php echo $baseUrl;?>report"><span class="glyphicon glyphicon-stats"></span> Thống kê</a></li>
-					<li><a href="<?php echo $baseUrl;?>auth"><span class="glyphicon glyphicon-user"></span> Đăng ký/Đăng nhập</a></li>
 				</ul>
 			</div>
 		</div>
