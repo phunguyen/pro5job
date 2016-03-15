f6be5fb81a30cc1b8ca748bea4fb0cb19b54d1df
Phu Nguyen <phului@gmail.com>
2016-03-16 00:07:53 +0700

    new db

--- db/pro5job.sql
+++ db/pro5job.sql
@@ -3,14 +3,14 @@ Navicat MySQL Data Transfer
 
 Source Server         : localhost
 Source Server Version : 50539
-Source Host           : :3306
+Source Host           : localhost:3306
 Source Database       : pro5job
 
 Target Server Type    : MYSQL
 Target Server Version : 50539
 File Encoding         : 65001
 
-Date: 2016-03-10 14:33:55
+Date: 2016-03-16 00:07:35
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,20 +21,22 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `asks`;
 CREATE TABLE `asks` (
   `ask_id` int(20) NOT NULL AUTO_INCREMENT,
-  `ask_name` varchar(255) NOT NULL,
+  `ask_name` varchar(255) CHARACTER SET latin1 NOT NULL,
   `ask_cat_id` int(10) DEFAULT NULL,
-  `description` text,
+  `description` text CHARACTER SET latin1,
+  `ask_name_en` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `description_en` text CHARACTER SET latin1,
   PRIMARY KEY (`ask_id`),
   KEY `ask_cat_id` (`ask_cat_id`),
   CONSTRAINT `asks_ibfk_1` FOREIGN KEY (`ask_cat_id`) REFERENCES `asks_cats` (`ask_cat_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of asks
 -- ----------------------------
-INSERT INTO `asks` VALUES ('1', 'ASK 1', '1', 'Ask 1 desc');
-INSERT INTO `asks` VALUES ('2', 'ASK 11', '1', 'd d d d');
-INSERT INTO `asks` VALUES ('3', 'ASK 2', '2', '22222');
+INSERT INTO `asks` VALUES ('1', 'ASK 1', '1', 'Ask 1 desc 1111', 'ASK EN 1', 'ask en en');
+INSERT INTO `asks` VALUES ('3', 'ASK 2', '2', '22222', 'ee22', 'eeeeee 2222222222');
+INSERT INTO `asks` VALUES ('4', 'Tên ti?ng vi?t', '3', 'Mô t?', 'Tên ti?ng anh', 'Mô t? ti?ng anh');
 
 -- ----------------------------
 -- Table structure for asks_cats
@@ -45,16 +47,17 @@ CREATE TABLE `asks_cats` (
   `ask_cat_name` varchar(255) NOT NULL,
   `description` text,
   `ask_cat_parent` int(10) DEFAULT '0',
+  `ask_cat_name_en` varchar(255) DEFAULT NULL,
+  `description_en` text,
   PRIMARY KEY (`ask_cat_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of asks_cats
 -- ----------------------------
-INSERT INTO `asks_cats` VALUES ('1', 'CAT 1', 'Cat 1 desc', '0');
-INSERT INTO `asks_cats` VALUES ('2', 'CAT 2', 'Cat 2 desc', '0');
-INSERT INTO `asks_cats` VALUES ('3', 'CAT 11', 'Cat 11 desc', '1');
-INSERT INTO `asks_cats` VALUES ('4', 'CAT 12', 'cat cat 1212', '1');
+INSERT INTO `asks_cats` VALUES ('1', 'CAT 1', 'CAT 1 desc', '0', 'ce1', 'ddddddddd eeeeeeeee 11111111');
+INSERT INTO `asks_cats` VALUES ('2', 'CAT 2', 'Cat 2 desc', '0', null, null);
+INSERT INTO `asks_cats` VALUES ('3', 'CAT 11', 'Cat 11 desc', '1', null, null);
 
 -- ----------------------------
 -- Table structure for groups
@@ -119,10 +122,10 @@ CREATE TABLE `users` (
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES ('1', '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', null, null, null, '1268889823', '1457594535', '1', 'User', 'Admin', 'ADMIN', '1112223333');
-INSERT INTO `users` VALUES ('2', '0.0.0.0', null, '$2y$08$SsBpg.wXD3ASR00dIYehP.JU6du8ekOCTq7.kbZd1gKOYTPmGDPLe', null, 'editor@editor.com', null, null, null, null, '1453985671', '1457593366', '1', 'User', 'Editor', 'Citigo', '0985819644');
-INSERT INTO `users` VALUES ('3', '0.0.0.0', null, '$2y$08$naYuAJIH42C.l.Pm45omh.r9VoKGoVN0xauHf4s1xvWZ85HLA.t/2', null, 'job@job.com', null, null, null, null, '1457017959', null, '1', 'User', 'Job', 'Boru', '111');
-INSERT INTO `users` VALUES ('4', '0.0.0.0', null, '$2y$08$4h1FhAvj2wKRaKBOzURV.em15IWg1MzIfOero07TdAGEqUI3BTq/u', null, 'profile@profile.com', null, null, null, null, '1457595123', '1457595144', '1', 'User', 'Profile', 'UP', '222');
+INSERT INTO `users` VALUES ('1', '127.0.0.1', 'administrator', '$2y$08$7rolBCnUvDi/2mVd371M5.q7o83TU12P4ibMRn/ZJJ27yRcrq.3fq', '', 'admin@admin.com', '', null, null, null, '1268889823', '1457706193', '1', 'User', 'Admin', 'ADMIN', '1112223333');
+INSERT INTO `users` VALUES ('2', '0.0.0.0', null, '$2y$08$uFWNFGTh4Jx4bZPUnmneHOtT0kA4I0SPObj0vNUvvYKTs4hq3.K2G', null, 'editor@editor.com', null, null, null, null, '1453985671', '1458060632', '1', 'User', 'Editor', 'Citigo', '0985819644');
+INSERT INTO `users` VALUES ('3', '0.0.0.0', null, '$2y$08$ZmmUwdf1XPOm/sNw2XLoM.Ot0fddxfcDwr9VIdhbi5QVgSumKVp2i', null, 'job@job.com', null, null, null, null, '1457017959', '1457706237', '1', 'User', 'Job', 'Boru', '111');
+INSERT INTO `users` VALUES ('4', '0.0.0.0', null, '$2y$08$A.iZFn1eHUOiVbEa18X0IOacUwr8JGZh47hjPE5Hd.q7ckm3R.nzm', null, 'profile@profile.com', null, null, null, null, '1457595123', '1457706261', '1', 'User', 'Profile', 'UP', '222');
 
 -- ----------------------------
 -- Table structure for users_groups
@@ -138,12 +141,12 @@ CREATE TABLE `users_groups` (
   KEY `fk_users_groups_groups1_idx` (`group_id`),
   CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of users_groups
 -- ----------------------------
-INSERT INTO `users_groups` VALUES ('9', '1', '1');
-INSERT INTO `users_groups` VALUES ('10', '2', '2');
-INSERT INTO `users_groups` VALUES ('11', '3', '3');
-INSERT INTO `users_groups` VALUES ('12', '4', '4');
+INSERT INTO `users_groups` VALUES ('13', '1', '1');
+INSERT INTO `users_groups` VALUES ('14', '2', '2');
+INSERT INTO `users_groups` VALUES ('15', '3', '3');
+INSERT INTO `users_groups` VALUES ('16', '4', '4');
