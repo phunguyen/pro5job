8146d6af99199d9bb5e97d371148bab57c99a0bf
Phu Nguyen <phului@gmail.com>
2016-04-05 16:02:48 +0700

    setting module

---
+++ application/modules/setting/controllers/setting.php
@@ -0,0 +1,26 @@
+<?php
+class Filter extends MX_Controller{
+
+	public function __construct() {
+		parent::__construct();
+        $this->load->helper(array("url"));
+	}
+
+	public function index() {
+		$this->template->write("title", "Tuyển dụng, Tìm việc, Đào tạo");
+        $this->template->write_view("content", "filter");
+        $this->template->render();
+	}
+
+	public function jobs() {
+		$this->template->write("title", "Tuyển dụng, Tìm việc, Đào tạo");
+        $this->template->write_view("content", "filter_jobs");
+        $this->template->render();
+	}
+
+	public function profiles() {
+		$this->template->write("title", "Tuyển dụng, Tìm việc, Đào tạo");
+        $this->template->write_view("content", "filter_profiles");
+        $this->template->render();
+	}
+}
---
+++ application/modules/setting/views/setting.php
@@ -0,0 +1,168 @@
+<div class="heading">
+		<div class="container">
+			<hr>
+			<div class="row">
+				<div class="col-md-3">
+				<form>
+					<input type="submit" value="Lưu Setting" class="btn btn-primary btn-block">
+				</form>
+				
+				</div>
+				<div class="col-md-9">
+				
+				</div>
+				
+			</div>
+			
+			
+		</div>
+	</div>
+	<hr>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+			<h3 style="text-align: left">Địa điểm</h3>
+			<hr>
+			<div class="row">
+					<div class="col-md-8">
+						<input type="text" class="form-control" name="location" placeholder="Địa điểm làm việc">
+					</div>	
+					<div class="col-md-4">
+						<input type="submit" value="Thêm" class="btn btn-primary btn-block">
+					</div>
+			</div>
+			<hr>
+				<table class="table table-condensed table-bordered">
+				<thead>
+					<tr>
+						<th>Tác vụ</th>
+						<th>Địa điểm</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>TP Hồ Chí Minh</td>
+					</tr>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>Hà Nội</td>
+					</tr>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>Đà Nẵng</td>
+					</tr>					
+				</tbody>
+			</table>
+			<hr>		
+			</div>
+			<div class="col-md-3">
+			<h3>Năm kinh nghiệm</h3>
+			<hr>
+				<div class="row">
+					<div class="col-md-8">
+						<input type="text" class="form-control" name="location" placeholder="Năm kinh nghiệm">
+					</div>	
+					<div class="col-md-4">
+						<input type="submit" value="Thêm" class="btn btn-primary btn-block">
+					</div>
+				</div>
+			<hr>
+			<table class="table table-condensed table-bordered">
+				<thead>
+					<tr>
+						<th>Tác vụ</th>
+						<th>Số năm kinh nghiệm</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>1 năm</td>
+					</tr>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>2 năm</td>
+					</tr>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>3 năm trở lên</td>
+					</tr>					
+				</tbody>
+			</table>
+			<hr>	
+			</div>
+			<div class="col-md-3">
+			<h3>Bằng cấp</h3>
+			<hr>
+				<div class="row">
+					<div class="col-md-8">
+						<input type="text" class="form-control" name="location" placeholder="Bằng cấp">
+					</div>	
+					<div class="col-md-4">
+						<input type="submit" value="Thêm" class="btn btn-primary btn-block">
+					</div>
+				</div>
+			<hr>
+			<table class="table table-condensed table-bordered">
+				<thead>
+					<tr>
+						<th>Tác vụ</th>
+						<th>Bằng cấp</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>Không yêu cầu</td>
+					</tr>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>Tốt nghiệp THCS</td>
+					</tr>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>Tốt nghiệp THPT</td>
+					</tr>					
+				</tbody>
+			</table>
+			<hr>	
+			</div>
+			<div class="col-md-3">
+			<h3>Mức lương</h3>
+			<hr>
+				<div class="row">
+					<div class="col-md-8">
+						<input type="text" class="form-control" name="location" placeholder="Mức lương">
+					</div>	
+					<div class="col-md-4">
+						<input type="submit" value="Thêm" class="btn btn-primary btn-block">
+					</div>
+				</div>
+			<hr>
+			<table class="table table-condensed table-bordered">
+				<thead>
+					<tr>
+						<th>Tác vụ</th>
+						<th>Mức lương</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>1-3 triệu</td>
+					</tr>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>3-5</td>
+					</tr>
+					<tr>
+						<td><a href="#">Sửa </a>| <a href="#">Xóa</a></td>
+						<td>Trên 25 triệu</td>
+					</tr>					
+				</tbody>
+			</table>
+			<hr>	
+			</div>
+		</div>
+	</div>
