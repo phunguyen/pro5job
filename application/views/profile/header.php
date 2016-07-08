a45a0538d74f2d7bb3d7389a077d81f28992aadd
baphong <baphong80@gmail.com>
2016-07-08 16:35:47 +0700

    them giao dien muc Gop y

--- application/views/profile/header.php
+++ application/views/profile/header.php
@@ -24,7 +24,29 @@ $baseUrl = base_url();
 					<li><a href="<?php echo $baseUrl;?>profile"><span class="glyphicon glyphicon-heart"></span> Profiles</a></li>
 					<li><a href="<?php echo $baseUrl;?>filter/jobs"><span class="glyphicon glyphicon-filter"></span> Lọc Jobs</a></li>
 					<li><a href="<?php echo $baseUrl;?>manage/jobs"><span class="glyphicon glyphicon-star"></span> Jobs</a></li>
-					<li><a href="<?php echo $baseUrl;?>auth"><span class="glyphicon glyphicon-comment"></span> Góp ý</a></li>
+					<li><a data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-comment"></span> Góp ý</a></li>
+					<div class="modal fade" id="myModal" role="dialogtabindex="-1" role="dialog">
+					<div class="modal-dialog" role="document">
+    
+						<!-- Modal content-->
+						<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<span style="font-size: 24px">Góp ý</span>
+							
+						</div>
+						<div class="modal-body">
+							
+							<textarea class="form-control" rows="7"></textarea>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-primary">Gửi góp ý</button>
+						</div>
+					</div>
+      
+					</div>
+					</div>
+					
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
 							<span class="glyphicon glyphicon-user"></span> <?php echo $current_user->email; ?><span class="caret"></span>
